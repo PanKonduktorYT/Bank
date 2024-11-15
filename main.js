5226f5f03861a08d19953c8e4f3a05c3abad4a84
Piotr Kruszec <pjkruszec@vip.interia.pl>
2024-11-15 13:50:45 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -9,3 +9,38 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 updateTime(); // Initial call to display the time immediately
+
+function getLocation() {
+    const locationElement = document.getElementById('location');
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        locationElement.innerHTML = "Geolokalizacja nie jest wspierana przez tę przeglądarkę.";
+    }
+}
+
+function showPosition(position) {
+    const locationElement = document.getElementById('location');
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    locationElement.innerHTML = `Szerokość geograficzna: ${latitude} <br> Długość geograficzna: ${longitude}`;
+}
+
+function showError(error) {
+    const locationElement = document.getElementById('location');
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            locationElement.innerHTML = "Użytkownik odmówił udostępnienia lokalizacji.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            locationElement.innerHTML = "Informacje o lokalizacji są niedostępne.";
+            break;
+        case error.TIMEOUT:
+            locationElement.innerHTML = "Przekroczono czas oczekiwania na lokalizację.";
+            break;
+        case error.UNKNOWN_ERROR:
+            locationElement.innerHTML = "Wystąpił nieznany błąd.";
+            break;
+    }
+}
